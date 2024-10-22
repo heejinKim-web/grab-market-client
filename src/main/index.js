99c63f097da5e15d8ec93404581e0640279a5c4e
heejin <desireoftruth1126@gmail.com>
2024-10-22 17:42:20 +0900

    상세페이지 구현 1

--- src/main/index.js
+++ src/main/index.js
@@ -21,43 +21,35 @@ function MainPage() {
 
   return (
     <div>
-      <div id="header">
-        <div id="header_area">
-          <img src="images/icons/logo.png" alt="로고" />
-        </div>
+      <div id="banner">
+        <img src="images/banners/banner1.png" alt="" />
       </div>
-      <div id="body">
-        <div id="banner">
-          <img src="images/banners/banner1.png" alt="" />
-        </div>
-        <h1>판매되는 상품들</h1>
-        <div id="product_list">
-          {products.map(function (product, index) {
-            return (
-              <div className="product_card" key={product.name}>
-                <Link to={"/products/" + index} className="product_link">
-                  <div>
-                    <img className="product_img" src={product.imageUrl} />
-                  </div>
-                  <div className="product-content">
-                    <span className="product_name">{product.name}</span>
-                    <span className="product_price">{product.price}원</span>
-                  </div>
-                  <div className="product_seller">
-                    <img
-                      className="product_avatar"
-                      src="images/icons/avatar.png"
-                      alt=""
-                    />
-                    <span>{product.seller}</span>
-                  </div>
-                </Link>
-              </div>
-            );
-          })}
-        </div>
+      <h1>판매되는 상품들</h1>
+      <div id="product_list">
+        {products.map(function (product, index) {
+          return (
+            <div className="product_card" key={product.name}>
+              <Link to={"/products/" + product.id} className="product_link">
+                <div>
+                  <img className="product_img" src={product.imageUrl} />
+                </div>
+                <div className="product-content">
+                  <span className="product_name">{product.name}</span>
+                  <span className="product_price">{product.price}원</span>
+                </div>
+                <div className="product_seller">
+                  <img
+                    className="product_avatar"
+                    src="images/icons/avatar.png"
+                    alt=""
+                  />
+                  <span>{product.seller}</span>
+                </div>
+              </Link>
+            </div>
+          );
+        })}
       </div>
-      <div id="footer"></div>
     </div>
   );
 }
