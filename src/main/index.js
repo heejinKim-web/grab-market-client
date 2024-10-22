0b6769f78404094c71da80bb0b75f0abb36d0eba
heejin <desireoftruth1126@gmail.com>
2024-10-22 16:44:42 +0900

    Link 태그 적용

--- src/main/index.js
+++ src/main/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./index.css";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 function MainPage() {
   const [products, setProducts] = React.useState([]);
@@ -34,21 +35,23 @@ function MainPage() {
           {products.map(function (product, index) {
             return (
               <div className="product_card" key={product.name}>
-                <div>
-                  <img className="product_img" src={product.imageUrl} />
-                </div>
-                <div className="product-content">
-                  <span className="product_name">{product.name}</span>
-                  <span className="product_price">{product.price}원</span>
-                </div>
-                <div className="product_seller">
-                  <img
-                    className="product_avatar"
-                    src="images/icons/avatar.png"
-                    alt=""
-                  />
-                  <span>{product.seller}</span>
-                </div>
+                <Link to={"/products/" + index} className="product_link">
+                  <div>
+                    <img className="product_img" src={product.imageUrl} />
+                  </div>
+                  <div className="product-content">
+                    <span className="product_name">{product.name}</span>
+                    <span className="product_price">{product.price}원</span>
+                  </div>
+                  <div className="product_seller">
+                    <img
+                      className="product_avatar"
+                      src="images/icons/avatar.png"
+                      alt=""
+                    />
+                    <span>{product.seller}</span>
+                  </div>
+                </Link>
               </div>
             );
           })}
